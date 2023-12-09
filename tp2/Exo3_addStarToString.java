56e6b0bbdf9b8e425f23f1f99c9192488cd76d21
tvinchent <tvinchent@gmail.com>
2023-12-09 16:35:00 +0100

    feat - tp2 exo3

---
+++ tp2/Exo3_addStarToString.java
@@ -0,0 +1,21 @@
+package tp2;
+
+import java.util.Scanner;
+
+public class Exo3_addStarToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String stringUser;
+        System.out.print("Entrez votre chaine de caractere");
+        Scanner lectureClavier = new Scanner(System.in);
+        stringUser = lectureClavier.nextLine();
+        lectureClavier.close();
+        
+        for(int i=0; i<stringUser.length(); i++) {
+        	String charToPrint = i % 2 == 0 ? String.valueOf(stringUser.charAt(i)) : "*";
+        	System.out.print(charToPrint);
+        }
+	}
+
+}
